fd8197f31d8a7f69e469dc268d39a3c3a50582cf
Lomeli12 <Lomeli12@users.noreply.github.com>
2015-06-22 13:19:39 -0700

    Fixed formatting from last PR.

--- src/main/java/net/lomeli/simplecondenser/tile/TileCondenserBase.java
+++ src/main/java/net/lomeli/simplecondenser/tile/TileCondenserBase.java
@@ -11,8 +11,11 @@ import net.minecraft.network.NetworkManager;
 import net.minecraft.network.Packet;
 import net.minecraft.network.play.server.S35PacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
+
 import net.minecraftforge.oredict.OreDictionary;
+
 import net.lomeli.lomlib.util.ItemUtil;
+
 import net.lomeli.simplecondenser.core.ModConfig;
 import net.lomeli.simplecondenser.lib.ItemLib;
 import net.lomeli.simplecondenser.lib.enums.AlchemicalType;
